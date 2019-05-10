08dafde715011ebdcc1946de76b5e72baf573bd4
estebanortegaroy <esteban@binpar.com>
2019-05-11 00:40:38 +0200

    initial commit

---
+++ build/precache-manifest.04d62fde4ed808073e211442f789c6f7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfad303e3017f0d3b4efab674cdf12dd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "175f2ee88282db61f6e5",
+    "url": "/static/css/main.15cba415.chunk.css"
+  },
+  {
+    "revision": "3fb0a46df6dac35b6533",
+    "url": "/static/js/2.1b6a7598.chunk.js"
+  },
+  {
+    "revision": "175f2ee88282db61f6e5",
+    "url": "/static/js/main.2de111f3.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
